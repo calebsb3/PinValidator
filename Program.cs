b746c3d4a3d02b2b15779befbb389be4a8510232
Caleb Belk <caleb8814@gmail.com>
2024-05-14 07:28:23 -0600

    Add digits validator

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using Xunit;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 namespace PinValidator;
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<IValidator> validators = new List<IValidator>
+        {
+            new LengthValidator(),
+            new JustDigitsValidator(),
+        };
         var lengthValidator = new LengthValidator();
         Assert.False(lengthValidator.IsValid("13579"));
     }
@@ -19,6 +26,14 @@ public class LengthValidator : IValidator {
     public int returnNum { get { return 2;}}
 
     public bool IsValid(string possiblePin){
-        return true;
+        return possiblePin.Length == 4 || possiblePin.Length == 6;
+    }
+}
+
+public class JustDigitsValidator: IValidator {
+    public int returnNum { get { return 3;}}
+
+    public bool IsValid(string possiblePin){
+        return Regex.IsMatch(possiblePin, @"^\d+$");
     }
 }
